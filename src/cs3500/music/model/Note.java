ebaf49f246ddc57a70f59cf2f83dd4020e1db4bb
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-06-07 10:30:23 -0400

    adds getters, toString, compareTo

--- src/cs3500/music/model/Note.java
+++ src/cs3500/music/model/Note.java
@@ -2,8 +2,62 @@ package cs3500.music.model;
 
 /**
  * Representation of an individual music note on a pitch.
- * Stores basic information such as start beat and duration.
+ * Stores basic information such as start beat and end beat (inclusive).
+ * Implements comparable to allow for sorting.
  */
-class Note {
+class Note implements Comparable<Note> {
+  private int start;
+  private int end;
 
+  /**
+   * Constructs an instance of a {@code Note}.
+   * @param start the start beat of the note (inclusive)
+   * @param end the end beat of the note (inclusive)
+   * @throws IllegalArgumentException if {@code start} or {@code end} are less than 0, or
+   *                                   if {@code end} is less than {@code start}
+   */
+  Note(int start, int end) throws IllegalArgumentException {
+    if (start < 0 || end < 0 || end < start) {
+      throw new IllegalArgumentException("Invalid start and/or end beats.");
+    }
+    this.start = start;
+    this.end= end;
+  }
+
+  /**
+   * Gets the start beat of this {@code Note}.
+   * @return the start beat
+   */
+  int getStart() {
+    return this.start;
+  }
+
+  /**
+   * Gets the end beat of this {@code Note}.
+   * @return the end beat
+   */
+  int getEnd() {
+    return this.end;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder s = new StringBuilder();
+    s.append("X");
+
+    for (int i = start + 1; i < end; i++) {
+      s.append("|");
+    }
+    return s.toString();
+  }
+
+  @Override
+  public int compareTo(Note n) {
+    if (this.start - n.start == 0) {
+      return this.end - n.end;
+    }
+    else {
+      return this.start - n.start;
+    }
+  }
 }
