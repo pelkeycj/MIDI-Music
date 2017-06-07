bb1684173e2b22348f096f9bd6f84b02e7e01df2
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-06-07 13:09:35 -0400

    interface

--- src/cs3500/music/model/MusicOperations.java
+++ src/cs3500/music/model/MusicOperations.java
@@ -1,12 +1,51 @@
 package cs3500.music.model;
 
-//TODO generic over notetype, octaveNumber?
+/**
+ * Interface to represent specifications for required methods for a music playler model.
+ */
 public interface MusicOperations {
-  //add note
 
-  // remove note
+  /**
+   * Adds a note from beats {@code start} to {@code end} at pitch
+   * specified by octave {@code o} and note type {@code nt}.
+   * @param o the octave to add to
+   * @param nt the note type to add to
+   * @param start the start beat of the note
+   * @param end the end beat of the note
+   */
+  void addNote(Octave o, NoteType nt, int start, int end);
 
-  //change note
+  /**
+   * Removes the note from beats {@code start} to {@code end} at the pitch
+   * specified by octave {@code o} and note type {@code nt}.
+   * @param o the octave to remove from
+   * @param nt the note type to remove from
+   * @param start the start of the note
+   * @param end the end of the note
+   * @throws IllegalArgumentException if {@code start}  or {@code end} are invalid, or there
+   *                                  is no {@code Note} to remove.
+   */
+  void removeNote(Octave o, NoteType nt, int start, int end) throws IllegalArgumentException;
 
-  // append
+  /**
+   * Changes the start or end beats for the specified note.
+   * @param o the octave to change
+   * @param nt the note type to change
+   * @param oldStart the original start position
+   * @param oldEnd the original end position
+   * @param newStart the new start position
+   * @param newEnd the new end position
+   * @throws IllegalArgumentException if there is no note to move, or one of the
+   *                                  location parameters are invalid.
+   */
+  void changeNote(Octave o, NoteType nt, int oldStart, int oldEnd, int newStart, int newEnd)
+          throws IllegalArgumentException;
+
+
+  /**
+   * Appends the sheet of notes represented in {@code m} to the end of this
+   * {@code MusicOperation}s sheet.
+   * @param m the sheet of notes to append
+   */
+  void appendSheet(MusicOperations m);
 }
