29d86a642e8a09870107a693a0c4cb630184864f
adambechtold <adamcbechtold@gmail.com>
2017-06-12 14:12:43 -0400

    added classes from starter code

---
+++ src/cs3500/music/view/GuiViewFrame.java
@@ -0,0 +1,35 @@
+package cs3500.music.view;
+
+import java.awt.*;
+import java.awt.event.MouseListener; // Possibly of interest for handling mouse events
+
+import javax.swing.*;
+
+/**
+ * A skeleton Frame (i.e., a window) in Swing
+ */
+public class GuiViewFrame extends javax.swing.JFrame implements YourViewInterfaceHere {
+
+  private final JPanel displayPanel; // You may want to refine this to a subtype of JPanel
+
+  /**
+   * Creates new GuiView
+   */
+  public GuiViewFrame() {
+    this.displayPanel = new ConcreteGuiViewPanel();
+    this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    this.getContentPane().add(displayPanel);
+    this.pack();
+  }
+
+  @Override
+  public void initialize(){
+    this.setVisible(true);
+  }
+
+  @Override
+  public Dimension getPreferredSize(){
+    return new Dimension(100, 100);
+  }
+
+}
---
+++ src/cs3500/music/view/MidiViewImplementation.java
@@ -0,0 +1,66 @@
+package cs3500.music.view;
+
+import javax.sound.midi.*;
+
+/**
+ * A skeleton for MIDI playback
+ */
+public class MidiViewImpl implements YourViewInterfaceHere {
+  private final Synthesizer synth;
+  private final Receiver receiver;
+
+  public MidiViewImpl() throws MidiUnavailableException {
+    this.synth = MidiSystem.getSynthesizer();
+    this.receiver = synth.getReceiver();
+    this.synth.open();
+  }
+  /**
+   * Relevant classes and methods from the javax.sound.midi library:
+   * <ul>
+   *  <li>{@link MidiSystem#getSynthesizer()}</li>
+   *  <li>{@link Synthesizer}
+   *    <ul>
+   *      <li>{@link Synthesizer#open()}</li>
+   *      <li>{@link Synthesizer#getReceiver()}</li>
+   *      <li>{@link Synthesizer#getChannels()}</li>
+   *    </ul>
+   *  </li>
+   *  <li>{@link Receiver}
+   *    <ul>
+   *      <li>{@link Receiver#send(MidiMessage, long)}</li>
+   *      <li>{@link Receiver#close()}</li>
+   *    </ul>
+   *  </li>
+   *  <li>{@link MidiMessage}</li>
+   *  <li>{@link ShortMessage}</li>
+   *  <li>{@link MidiChannel}
+   *    <ul>
+   *      <li>{@link MidiChannel#getProgram()}</li>
+   *      <li>{@link MidiChannel#programChange(int)}</li>
+   *    </ul>
+   *  </li>
+   * </ul>
+   * @see <a href="https://en.wikipedia.org/wiki/General_MIDI">
+   *   https://en.wikipedia.org/wiki/General_MIDI
+   *   </a>
+   */
+
+  public void playNote() throws InvalidMidiDataException {
+    MidiMessage start = new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 64);
+    MidiMessage stop = new ShortMessage(ShortMessage.NOTE_OFF, 0, 60, 64);
+    this.receiver.send(start, -1);
+    this.receiver.send(stop, this.synth.getMicrosecondPosition() + 200000);
+
+    /*
+    The receiver does not "block", i.e. this method
+    immediately moves to the next line and closes the
+    receiver without waiting for the synthesizer to
+    finish playing.
+
+    You can make the program artificially "wait" using
+    Thread.sleep. A better solution will be forthcoming
+    in the subsequent assignments.
+    */
+    this.receiver.close(); // Only call this once you're done playing *all* notes
+  }
+}
