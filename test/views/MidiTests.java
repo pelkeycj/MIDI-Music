68e05d177c7e6a002706a15ec85070ab253a0dff
adambechtold <adamcbechtold@gmail.com>
2017-06-16 15:20:43 -0400

    adjusted audio tests

--- test/views/MidiTests.java
+++ test/views/MidiTests.java
@@ -1,19 +1,24 @@
 package views;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
+import cs3500.music.control.IController;
+import cs3500.music.control.SimpleController;
 import cs3500.music.model.MusicOperations;
 import cs3500.music.model.MusicSheet;
 import cs3500.music.model.NoteTypeWestern;
 import cs3500.music.model.OctaveNumber0To10;
+import cs3500.music.util.CompositionBuilder;
+import cs3500.music.util.MusicReader;
+import cs3500.music.util.SheetBuilder;
 import cs3500.music.view.IView;
 import cs3500.music.view.AudioView;
-import java.util.ArrayList;
-import java.util.List;
-import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiMessage;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.sound.midi.ShortMessage;
+import javax.swing.Icon;
 import org.junit.Test;
 
 /**
@@ -50,8 +55,8 @@ public class MidiTests {
     s.append("Command: ").append(ShortMessage.NOTE_OFF).append(" ");
     s.append("Channel: ").append(channel).append(" ");
     s.append("Data1: ").append(pitch).append(" ");
-    s.append("Data2: ").append(loudness).append("\n");
-    s.append("Timestamp: ").append(-1).append("\n");
+    s.append("Data2: ").append(loudness).append(" ");
+    s.append("Timestamp: ").append(duration * 1000000).append("\n");
     return s.toString();
   }
 
@@ -75,17 +80,38 @@ public class MidiTests {
   }
 
   @Test
-  public void twoNoteTest() {
+  public void twoNoteTest() throws IOException {
     model = new MusicSheet();
     StringBuilder expectedLog = new StringBuilder();
 
     model.addNote(OctaveNumber0To10.O3, NoteTypeWestern.B, 30, 40, 2, 20);
     model.addNote(OctaveNumber0To10.O1, NoteTypeWestern.C, 1, 3, 1, 25);
 
-    expectedLog.append(addMidiStartStop(0, 12, 25, 10));
-    expectedLog.append(addMidiStartStop(0, 47, 20, 3));
+    expectedLog.append(addMidiStartStop(0, 12, 25, 2));
+    expectedLog.append(addMidiStartStop(0, 47, 20, 10));
 
     runAudio(model, expectedLog.toString());
+  }
+
+  public void createMaryLittleLambTranscript() {
+    StringBuilder log = new StringBuilder();
+    MusicOperations model = new MusicSheet();
+    IController controller = new SimpleController(model, AudioView.buildTestView(log));
+
+    String filename = "res/mary-little-lamb.txt";
+
+    CompositionBuilder<IController> builder = new SheetBuilder(controller);
+    try {
+      MusicReader.parseFile(new FileReader(filename), builder).go();
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException("File failed to open");
+    }
+
+    try(  PrintWriter out = new PrintWriter( "midi-transcript.txt" )  ){
+      out.println( log.toString() );
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException("File failed to open");
+    }
   }
 
 }
