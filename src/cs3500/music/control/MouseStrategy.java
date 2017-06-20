5a501cb18e548a47521e6d1e1a861a61cda66f2e
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-06-20 14:01:51 -0400

    mousehandling

--- src/cs3500/music/control/MouseStrategy.java
+++ src/cs3500/music/control/MouseStrategy.java
@@ -6,38 +6,15 @@ import java.util.Map;
 /**
  * Interface to specify operations that must be performed
  * by a mouse listener in the Music Editor program to handle
- * user mouse input.
+ * user mouse input. Generic over the type of class to handle mouse events.
  */
-public interface MouseStrategy extends MouseListener {
+public interface MouseStrategy<T> extends MouseListener {
 
   /**
-   * Sets the mouse click strategy.
-   * @param map mouse click strategy
+   * Sets the mouse event strategy. Mapped from an integer represenation of a mouse event
+   * to a type T representing a class to handle input.
+   * @param map mouse event strategy
    */
-  void setMouseClicks(Map<Integer, Runnable> map);
-
-  /**
-   * Sets the mouse presses strategy.
-   * @param map mouse presses strategy
-   */
-  void setMousePresses(Map<Integer, Runnable> map);
-
-  /**
-   * Sets the mouse enters strategy.
-   * @param map mouse enters strategy
-   */
-  void setMouseEnters(Map<Integer, Runnable> map);
-
-  /**
-   * Sets the mouse exits strategy.
-   * @param map mouse exits strategy
-   */
-  void setMouseExits(Map<Integer, Runnable> map);
-
-  /**
-   * Sets the mouse releases strategy.
-   * @param map mouse releases strategy
-   */
-  void setMouseReleased(Map<Integer, Runnable> map);
+  void setMouseEvents(Map<Integer, T> map);
 
 }
